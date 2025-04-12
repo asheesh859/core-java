6235cdad30931f65e9ecc2fcbec05a2e59c52d35
asheesh859 <ak2305@checkboxtechnology.com>
2025-04-12 18:58:27 +0530

    functional interface and lambda 12/04/2025 06:58 PM

--- Core_java_Basic/src/lambda_expression/returnFromInterface.java
+++ Core_java_Basic/src/lambda_expression/returnFromInterface.java
@@ -5,9 +5,9 @@ public class returnFromInterface {
 	public static void main(String[] args) {
 
 		return_interface r1 = (int x, int y) -> {
-			return x+y;
+			return x + y;
 		};
-		
+
 		int sum = r1.test(200, 100);
 		System.out.println(sum);
 	}
