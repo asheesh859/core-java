6309dfed2d1339721a57db4c6b15c0b41907e873
asheesh859 <ak2305@checkboxtechnology.com>
2025-04-17 22:45:22 +0530

    exception 17/04/2025 10:45 PM

--- Core_java_Basic/src/final_Keyword/E.java
+++ Core_java_Basic/src/final_Keyword/E.java
@@ -1,7 +1,5 @@
 package final_Keyword;
 
-import lambda_expression.return_interface;
-
 public class E {
 	
 
@@ -11,9 +9,11 @@ public class E {
 			int b = 0;
 			int result = a / b;
 			System.out.println(result);
+			
 		} 
 		catch (Exception e) {
 			System.out.println("error exception occur : "+e.getMessage());
+			e.printStackTrace();
 		}
 		
 		finally {
