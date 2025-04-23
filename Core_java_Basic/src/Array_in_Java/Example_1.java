2632670da0fc474a9888f982178ac03cc621238f
asheesh859 <ak2305@checkboxtechnology.com>
2025-04-23 23:30:09 +0530

    Array

---
+++ Core_java_Basic/src/Array_in_Java/Example_1.java
@@ -0,0 +1,17 @@
+package Array_in_Java;
+
+public class Example_1 {
+
+	public static void main(String[] args) {
+		int arr[] = new int[3];
+		
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
