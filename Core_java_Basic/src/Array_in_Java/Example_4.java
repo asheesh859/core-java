10c9143393f00f23dc53822499c14c7c4ed83e0a
asheesh859 <ak2305@checkboxtechnology.com>
2025-04-23 23:41:05 +0530

    Array 23/04/2025 11:40 PM

---
+++ Core_java_Basic/src/Array_in_Java/Example_4.java
@@ -0,0 +1,14 @@
+package Array_in_Java;
+
+public class Example_4 {
+	//called dynamic size of array 
+	public static void main(String[] args) {
+		int arr[]= {10,20,30,40,50};
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+}
