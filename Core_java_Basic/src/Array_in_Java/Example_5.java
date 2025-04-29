34eb9110cc9d5ff0b76f2bf5f3e9403e46cb2d97
asheesh859 <ak2305@checkboxtechnology.com>
2025-04-29 23:58:40 +0530

    .

--- Core_java_Basic/src/Array_in_Java/Example_5.java
+++ Core_java_Basic/src/Array_in_Java/Example_5.java
@@ -4,22 +4,28 @@ public class Example_5 {
 
 		public static void main(String[] args) {
 			int arr[]= {10,30,30,66,66,70,80,80};
-			int temp[]= {};
+			int temp[]= new int[arr.length];
 			int j=0;
 			
-			for(int i=0; i<arr.length; i++) {
-				for(i=i+1; i<arr.length; i++ ) {
+			for(int i=0; i<arr.length-1; i++) 
+				{
 					if(arr[i]!=arr[i+1]) {
-						temp[j++]=arr[i];
-						
+						temp[j]=arr[i];
+						j++;
+						j++;
 						
 					}
-				}
+				
 				
 			}
-			for(int i=0; i<temp.length; i++) {
-				System.out.println(temp[i]);
+				temp[j]=arr[arr.length-1];
+				
+			for(int t:temp) {
+				if(t==0) {
+					break;
+				}
+				System.out.println(t);
 			}
 		}
 
-}
+		}
