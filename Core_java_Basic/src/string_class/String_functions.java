3a80cc704748effe80b0ced215c10a9867802752
asheesh859 <ak2305@checkboxtechnology.com>
2025-05-23 17:12:42 +0530

    string functions 23/05/2025 5:12 PM

---
+++ Core_java_Basic/src/string_class/String_functions.java
@@ -0,0 +1,11 @@
+package string_class;
+
+public class String_functions {
+
+	public static void main(String[] args) {
+		String string = "hello world";
+		System.out.println(string.endsWith("w"));
+		System.out.println(string.startsWith("h"));
+	}
+
+}
