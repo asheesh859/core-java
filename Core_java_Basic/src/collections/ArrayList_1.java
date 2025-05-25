f70c6c3e4cdc249cd5631d77b31cfb720cffa197
asheesh859 <ak2305@checkboxtechnology.com>
2025-05-25 12:47:24 +0530

    Collection ArrayList 25/05/2025 12:47 PM

---
+++ Core_java_Basic/src/collections/ArrayList_1.java
@@ -0,0 +1,34 @@
+package collections;
+
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+public class ArrayList_1 {
+
+	public static void main(String[] args) {
+		ArrayList<String> arrayList = new ArrayList<>();
+		
+		arrayList.add("USA");
+		arrayList.add("India");
+		arrayList.add("Germany");
+	
+		System.out.println(arrayList);
+		
+		System.out.println(arrayList.size());
+		arrayList.add(2,"Slovakia");
+		
+		System.out.println(arrayList);
+		
+		arrayList.remove(2);
+		System.out.println(arrayList);
+		arrayList.set(2, "UAE");
+		
+		
+		System.out.println(arrayList);
+		
+		System.out.println(arrayList.get(1));
+		
+	}
+
+}
