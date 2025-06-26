6e1aad36696fc8f65a22f55bba14c13a205fa7b2
asheesh859 <ak2305@checkboxtechnology.com>
2025-06-26 17:51:13 +0530

    mapToint function in stream 26/06/2025 5:51 PM

--- Core_java_Basic/src/StreamAPI/MappingOprations.java
+++ Core_java_Basic/src/StreamAPI/MappingOprations.java
@@ -6,9 +6,17 @@ import java.util.List;
 public class MappingOprations {
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("India" , "Uk" ,"Japan" ,"China" , "Nepal" , "Slovakia" ,"Hangry" , "Jermany");
+		
 		list.stream().map(name -> name.toUpperCase()).forEach(country -> System.out.println(country));
+		
 		System.out.println("*******************");
 		list.stream().filter(name -> name.startsWith("J")).forEach(country -> System.out.println(country));
+		
+		System.out.println("********************");
+		list.stream().mapToInt(name -> name.length()).forEach(county -> System.out.println(county));
+		
+		System.out.println("********************");
+		list.stream().filter(p -> p.startsWith("J")).map(f -> f +"-"+ f.length()).forEach(country -> System.out.println(country));
 	}
 
 }
