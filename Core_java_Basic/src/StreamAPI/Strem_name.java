560f2a9da9ef068863e33ad64c0f6b571631a3f5
asheesh859 <ak2305@checkboxtechnology.com>
2025-06-26 17:07:46 +0530

    sorting number and name using stream API with filter function 26/06/2025 5:07 PM

--- Core_java_Basic/src/StreamAPI/Strem_name.java
+++ Core_java_Basic/src/StreamAPI/Strem_name.java
@@ -6,16 +6,15 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class Strem_name {
-public static void main(String[] args) {
-	
-	List<Integer> list = Arrays.asList(66,32,45,12,30);
-	
-	list.stream().filter(e -> e>20).forEach( i -> System.out.println(i));
-	
-	List<String> nameStrings = Arrays.asList("John" , "Ashu" , "Abhishek " , "Anurag" , "Joshi " , "Mike" , "Kirti");
-	
-	nameStrings.stream().filter(startWithA -> startWithA.startsWith("A")).forEach(name -> System.out.println(name));
-	
-	
-}
+	public static void main(String[] args) {
+
+		List<Integer> list = Arrays.asList(66, 32, 45, 12, 30);
+
+		list.stream().filter(e -> e > 20).forEach(i -> System.out.println(i));
+
+		List<String> nameStrings = Arrays.asList("John", "Ashu", "Abhishek ", "Anurag", "Joshi ", "Mike", "Kirti");
+
+		nameStrings.stream().filter(startWithA -> startWithA.startsWith("A")).forEach(name -> System.out.println(name));
+
+	}
 }
